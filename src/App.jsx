510daa5372156c8abb1985f32488d34f344ff78f
Abraham zeal <abrahamzeal72@gmail.com>
2025-08-14 15:51:48 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import LandingPage from './LandingPage.jsx';
+import QRCodeGenerator from './components/business/pages/QrGenerator.jsx';
 import ScanReviewLanding from '../src/components/business/pages/BusinessAuth.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/" element={<LandingPage />} /> */}
+        <Route path="/" element={<QRCodeGenerator />} />
         <Route path="/businessAuth" element={<ScanReviewLanding />} />
         {/* <Route path="/signin" element={<SignInPage />} /> */}
         {/* <Route path="/review" element={<ReviewPage />} /> */}
